4c5f3f82b46d1dab9d2b29dcf1d1a87d06ca9c20
Robert Benson <rbenson@bandwidth.com>
2019-02-02 00:29:03 -0500

    version that works with stdin

--- ccaatt.c
+++ ccaatt.c
@@ -1,33 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
 
-static FILE *stdin_file = NULL;
+// Gnlike actual cat we're not going to do formatting options and stuff, at least for now,
+// so just do something like the simple_cat function, but also don't care about dest file
+// block size, for simplicity
+#define INBUF_SIZE 1024
+#define OUTBUF_SIZE 2048
 
+static void copy_stdin(void);
 
-static FILE* open_stdin(void);
-static void close_stdin(void);
-
-static void close_regular_file(FILE *file, const char *filename);
-
-static void do_close_file(FILE *file, const char *filename);
+static void close_file(FILE *file, const char *filename);
 
 static void copy_file(FILE *file, const char *name);
 
 int main(int argc, char *argv[])
 {
 	if (argc < 2) {
-		copy_file(open_stdin(), "stdin");
+		copy_stdin();
 	} else {
 		for (int i = 1; i < argc; i++) {
 			if ('-' == argv[i][0] && 0 == argv[i][1]) {
-				copy_file(open_stdin(), "stdin");
+				copy_stdin();
 			} else {
 				copy_file(fopen(argv[i], "r"), argv[i]);
 			}
 		}
 	}
-	close_stdin(); // iff it was opened
 }
 
 static void copy_file(FILE *file, const char *name)
@@ -47,35 +47,37 @@ static void copy_file(FILE *file, const char *name)
 		WRITE_CHAR(c);
 	}
 #undef WRITE_CHAR
-	close_regular_file(file, name);
+	close_file(file, name);
 }
 
-static void close_regular_file(FILE *file, const char *filename)
-{
-	if (file != stdin_file) {
-		do_close_file(file, filename);
-	}
-}
-
-static FILE *open_stdin(void)
-{
-	if (NULL == stdin_file) {
-		stdin_file = fdopen(0, "r");
-	}
-	return stdin_file;
-}
-
-static void close_stdin(void)
-{
-	if (NULL != stdin_file) {
-		do_close_file(stdin_file, "stdin");
-	}
-}
-
-static void do_close_file(FILE *file, const char *filename)
+static void close_file(FILE *file, const char *filename)
 {
 	if (EOF == fclose(file)) {
 		dprintf(2, "errno %d: could not close file '%s'\n", errno, filename);
 		exit(1);
 	}
+}
+
+static void copy_stdin(void)
+{
+	char inbuf[INBUF_SIZE], outbuf[OUTBUF_SIZE];
+	int nread = 0;
+	int nwritten = 0;
+	do {
+		nread = read(0, inbuf, INBUF_SIZE);
+		if (-1 == nread) {
+			perror("failed to read from stdin");
+			exit(1);
+		}
+		if (0 == nread) break;
+		for (int i = 0; i < nread; i++) {
+			outbuf[2 * i] = inbuf[i];
+			outbuf[1 + 2 * i] = inbuf[i];
+		}
+		nwritten = write(1, outbuf, 2 * nread);
+		if (nwritten < nread) {
+			perror("error writing output");
+			exit(1);
+		}
+	} while (1);
 }
